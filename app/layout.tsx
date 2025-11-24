7f764553f2af0850e372f192d859db0efbcfda67
Muhammad Fayyih Zamhariry <sinyoxera@gmail.com>
2025-11-24 10:32:03 +0700

    fix notification

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
-        <Toaster position="top-right" richColors closeButton />
+        <Toaster position="top-right" richColors />
       </body>
     </html>
   );
